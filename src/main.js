df287b96509cfbb41ac0e3da46197482ff9f231d
RuiderEdgar <edgar.riveraj@gmail.com>
2024-02-15 03:15:53 -0600

    encrypt function and copy button finished

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 //buttons
 const buttonEcriptar = document.querySelector('.encriptar')
 const buttonDesencriptar = document.querySelector('.desencriptar')
+const buttonCopiar = document.querySelector('.copiar')
 
 
 const cifrado = {
@@ -11,8 +12,23 @@ const cifrado = {
     u: 'ufat'
 }
 
-//mostrar
-
+//mostrar mensaje encriptado
+const showMessage = (message) => {
+    //ocultar el texto principal
+    const messageNotFound = document.querySelector(".messageNotFound");
+    const description = document.querySelector(".description");
+    messageNotFound.style.display = "none" 
+    description.style.display = "none" 
+    
+    //mostrar el texto cifrado
+    const messageBox = document.querySelector(".message")
+    messageBox.innerHTML = `${message}`
+    messageBox.style.display = 'block'
+    
+    //mostrar boton copiar
+    const copyButton = document.querySelector(".copiar")
+    copyButton.style.display='inline-block';
+}
 
 //encriptacion
 const encriptar = (text) => {
@@ -44,15 +60,26 @@ const encriptar = (text) => {
 
     //regresando a string
     const newText = arrayText.join('')
-    console.log(newText)
+    showMessage(newText);
 }
 
 //obtener el texto del textarea
 const getText = () => {
     let text = document.querySelector('textarea').value;
-    console.log(text)
     encriptar(text)
+}
+
+//funcion para copiar al portapapeles
+const copyToClipboard = () => {
+    let message = document.querySelector(".message").innerText;
+    console.log(message)
+    navigator.clipboard.writeText(message).then(()=>{
+        alert("Mensaje copiado al portapapeles");
+    }).catch(err => {
+        console.log('Error al copiar el mensaje')
+    })
 }
 
 //eventos
 buttonEcriptar.addEventListener('click', getText)
+buttonCopiar.addEventListener( "click", copyToClipboard);
